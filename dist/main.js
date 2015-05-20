99565297017e5cca4ad23c13d9ee9ba4ddcf0b5c
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-05-20 14:42:20 -0500

    clock is working, no colors

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,23 @@
-function () {
+var colorClock = document.getElementById('clock');
+
+var updateClock = setInterval(function() {
+	var now = new Date();
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	var seconds = now.getSeconds();
 	
-}
-
-
+	if(hours > 12) {
+		hours = hours - '12';
+		hours = '0' + hours;
+	}
+	if(minutes <= 9) {
+		minutes = '0'+minutes;
+	}
+	if(seconds <= 9) {
+		seconds = '0'+seconds;
+	}
+	colorClock.innerHTML = hours + ':' + minutes + ':' + seconds;
+}, 500);
 
 
 
